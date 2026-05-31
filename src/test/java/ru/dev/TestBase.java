5f81eb25cf8d1f05f4491030f8a2e85418b12383
Neobutova.SI@parmalogica.ru <Neobutova.SI@parmalogica.ru>
2026-05-31 22:18:48 +0500

    параметризация в  jenkins

--- src/test/java/ru/dev/TestBase.java
+++ src/test/java/ru/dev/TestBase.java
@@ -17,20 +17,24 @@ public class TestBase {
     protected static TextBoxPage textBoxPage = new TextBoxPage();
 
     @BeforeAll
-    static void configure() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+    static void beforeAll() {
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "true"));
+        Configuration.pageLoadStrategy = System.getProperty("pageLoadStrategy", "normal");
         Configuration.timeout = 10000;
-        Configuration.pageLoadStrategy = "normal";
         Configuration.browserPosition = "0x0";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("remote");
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+        capabilities.setCapability("browserVersion", Configuration.browserVersion);
+        capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
-                "enableVideo", true
+                "enableVideo", true,
+                "screenResolution", Configuration.browserSize
         ));
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
